69e18c63bcb9cc54fe69bbac476252821d8ad17f
SylviaRemington <sylviaremington@gmail.com>
2025-05-07 11:40:32 -0700

    correcting syntax of objects in data.js

--- data.js
+++ data.js
@@ -4,40 +4,29 @@
 //objects have key:valuePairs,
 //create a randomization of pulling these comments. Figure out how to do that in JS.
 const carComments = [
-    { comment1: "Oh noooo! I was driving to slow!"},
-    {comment2: "",},
-    {},
-    {},
-    {},
-    {},
-
-    }
-        
-        comment3: "",
-        comment4: "",
-        comment5: "",
-    }
+    { comment1: "Oh noooo! I was driving to slow!" },
+    { comment2: "" },
+    { comment3: "" },
+    { comment4: "", },
+    { comment5: "", },
 ]
 
-const congratsComments = [
-    {
-        congrats1: "You're technique is very strong!",
-        congrats2: ""
-    }
 
+const congratsComments = [
+    { congrats1: "You're technique is very strong!" },
+    { congrats2: "" },
+    { congrats3: "" },
+    { congrats4: "" },
+    { congrats5: "" },
 ]
 
 
 const gameOverComments = [
-    { 
-        gameOverComment1: "Gameover, man! Gameover!", //from the movie Aliens2. Maybe also add "Why don't you put her in charge?!"
-        gameOverComment2: "Dooooooooooooooode",
-        gameOverComment3: "",
-        gameOverComment4: "",
-        gameOverComment5: "",
-    
-    
-    }
+    {gameOverComment1: "Gameover, man! Gameover!"}, //from the movie Aliens2. Maybe also add "Why don't you put her in charge?!"
+    {gameOverComment2: "Dooooooooooooooode"},
+    {gameOverComment3: ""},
+    {gameOverComment4: ""},
+    {gameOverComment5: ""},  
 ]
 
 
@@ -45,6 +34,7 @@ const carChoiceNinja = {
     name: "Ferrari Ninja",
     speed: 120, //or could do a string "The speed of sound."
     weapons: ["Sword", "Ninja Stars/Throwing Stars", "Nunchucks"],
+    location: Rome, //where car typically likes to drive
 }
 //above create a startGame function within the Object like...
 // startGame: function() {
