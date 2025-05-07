4a6209c5b0097b26b648c0b7aab2da6bcc3fbe6d
SylviaRemington <sylviaremington@gmail.com>
2025-05-07 02:28:32 -0700

    adding data.js file for carComments and congratsComments

---
+++ data.js
@@ -0,0 +1,21 @@
+//data for putting car comments and also congratulatory comments
+
+
+//objects have key:valuePairs,
+const carComments = [
+    {
+        comment1: "Oh noooo! I was driving to slow!",
+        comment2: "",
+        comment3: "",
+        comment4: "",
+        comment5: "",
+    }
+]
+
+const congratsComments = [
+    {
+        congrats1: "You're technique is very strong!",
+        congrats2: ""
+    }
+
+]
