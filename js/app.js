0417e1ba97dbd988acb5d6abdfe5a6dd31167f9b
SylviaRemington <sylviaremington@gmail.com>
2025-05-09 14:27:14 -0700

    additional syntax added in comments to better understand JS as I code - moved to code graveyard

--- js/app.js
+++ js/app.js
@@ -410,10 +410,60 @@ If you want more complex mechanics like obstacles or combo scoring
 
 // ---------------------------------------
 
+//ADDTL INFORMATION BELOW ABOUT EACH SECTION AND EXAMPLES OF SAMPLE CODE TO REFERENCE IN CASE I NEED TO REMEMBER SYNTAX FOR JS
 
+/*-------------------------------- Constants --------------------------------*/
+//for creating a new element and saving it in memory
+//e.g. const clickToAddParagraphButton = document.createElement('button')
+
+
+/*-------------------------------- Variables --------------------------------*/
+
+// let pointsEarned = '';
+
+// Store score/lives in variables (score, lives).
+
+
+/*------------------------ Cached Element References ------------------------*/
+//Things I want to select so I can do something with them.
+//e.g. const button = document.querySelector('button');
+//e.g. const title = document.querySelector ('h1');
+
+//button to start game
+//button to adjust game speed
+//button for fun facts --randomized
+//selecting points earned area (might not be a button but something else)
+//click on cars to create getting points (can you create a car as a button? Or an image?)
+
+
+/*-------------------------------- Functions --------------------------------*/
+//What it should do... e.g. text in the title should update
+//e.g. function updateTitle(){
+        // title.textContent = "Updated"}; 
+
+//Create a function that connects to a different page to start the game
+//Create a function that randomizes fun facts
+//Create a function to add points earned
+//Create a function that gives you instructions when you click on the Basic Instructions button
+
+// USE THIS:
+// updateScore() and updateLives() functions
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+//e.g. button.addEventListener('click', updateTitle); //when you click on the button, it will update the title
+
+//listen for clicks to create instructions
+//listen for clicks to start game //Do they all have to be separate event listeners?
+//listen for click to adjust game speed
+
+//The part showing starting and ending game
+//The part showing the "playing of the game/gameplay"
+//Make subfunction categories - like button functions/ ninja functions/ word bubble functions/ sound functions
 
 
 */
+
 
 // DATA.JS CODE GRAVEYARD / // JS CODE GRAVEYARD /*----------------------------- DATA.JS CODE GRAVEYARD  -----------------------------*/
 // DATA.JS CODE GRAVEYARD / // JS CODE GRAVEYARD /*----------------------------- DATA.JS CODE GRAVEYARD  -----------------------------*/
