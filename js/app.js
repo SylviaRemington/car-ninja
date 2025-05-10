f4d7406e3fd5961f9b557d538723cba96d72f682
SylviaRemington <sylviaremington@gmail.com>
2025-05-09 17:42:23 -0700

    creating cached element references for the game

--- js/app.js
+++ js/app.js
@@ -71,7 +71,23 @@ createGrid();
 //button for fun facts --randomized
 //selecting points earned area (might not be a button but something else)
 //click on cars to create getting points (can you create a car as a button? Or an image?)
+adjust-game-speed
+pause-game
+leave-game
+fun-facts
 
+const startGameButton = document.querySelector('#start-game');
+const instructionsButton = document.querySelector('#instructions');
+const gameGrid = document.querySelector('#game-grid');
+const score = document.querySelector('#points-earned');
+const lives = document.querySelector('#tries-left');
+
+const screens = {
+  start: document.querySelector('#start-screen'),
+  instructions: document.querySelector('#instructions-screen'),
+  gameplay: document.querySelector('#gameplay-screen'),
+  gameover: document.querySelector('#gameover-screen')
+};
 
 
 
